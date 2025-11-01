d7256b4d2ae0d2f65cc25944de9ca9f7af097210
aoctaviop <aoctaviop@gmail.com>
2025-11-01 12:14:07 -0600

    Initial commit

---
+++ HWCodingChallenge/HWCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWCodingChallengeApp.swift
+//  HWCodingChallenge
+//
+//  Created by Octavio on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HWCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
