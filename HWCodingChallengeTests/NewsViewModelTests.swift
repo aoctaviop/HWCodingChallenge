5f3cfcc1d1459df9ef7d8796aaed48edd924144a
aoctaviop <aoctaviop@gmail.com>
2025-11-01 17:45:07 -0600

    Updates tests for NewsViewModel

--- HWCodingChallengeTests/NewsViewModelTests.swift
+++ HWCodingChallengeTests/NewsViewModelTests.swift
@@ -11,7 +11,6 @@ import XCTest
 
 final class NewsViewModelTests: XCTestCase {
 
-    
     @MainActor
     func testNewsViewModel_ChangeCategorySavesNewSelectedCategory() async throws
     {
@@ -20,15 +19,15 @@ final class NewsViewModelTests: XCTestCase {
                 networkClient: try NetworkClientStub.make()
             )
         )
-        
+
         viewModel.category = .science
-        
+
         let savedCategory =
-        UserDefaults.standard.string(forKey: Constants.Keys.category) ?? ""
-        
+            UserDefaults.standard.string(forKey: Constants.Keys.category) ?? ""
+
         XCTAssertEqual(Category.science.rawValue, savedCategory)
     }
-    
+
     @MainActor
     func testNewsViewModel_FetchNewsRetrievesNews() async throws {
         let news: [Article] = [
@@ -45,6 +44,42 @@ final class NewsViewModelTests: XCTestCase {
         try? await viewModel.fetchNews()
 
         XCTAssertEqual(viewModel.articles.count, news.count)
+    }
+
+    @MainActor
+    func
+        testNewsViewModel_ShowingGeneralFeedShouldBeFalseWhenAnyCategoryIsSelected()
+        async throws
+    {
+        let viewModel = NewsViewModel(
+            newsService: NewsService(
+                networkClient: try NetworkClientStub.make()
+            )
+        )
+
+        viewModel.category = .science
+
+        XCTAssertFalse(viewModel.isShowingGeneralFeed())
+    }
+
+    @MainActor
+    func
+        testNewsViewModel_ShowingGeneralFeedShouldBeTrueWhenAllCategoryIsSelected()
+        async throws
+    {
+        let viewModel = NewsViewModel(
+            newsService: NewsService(
+                networkClient: try NetworkClientStub.make()
+            )
+        )
+
+        XCTAssertTrue(viewModel.isShowingGeneralFeed())
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        UserDefaults.standard.removeObject(forKey: Constants.Keys.category)
     }
 
 }
