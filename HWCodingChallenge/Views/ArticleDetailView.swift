76edad266cc78390797b14a03b8a8db4bd7196ab
aoctaviop <aoctaviop@gmail.com>
2025-11-01 22:42:56 -0600

    Includes support for iPad in the detail view.

--- HWCodingChallenge/Views/ArticleDetailView.swift
+++ HWCodingChallenge/Views/ArticleDetailView.swift
@@ -11,110 +11,106 @@ struct ArticleDetailview: View {
     var viewModel: ArticleViewModel
 
     var body: some View {
-        NavigationView {
-            GeometryReader { geometry in
-                let maxWidth = geometry.size.width
-                let maxHeight = geometry.size.height * 0.4
-                VStack {
-                    ZStack(alignment: .bottom) {
-                        if let imageURL = viewModel.getImageURl() {
-                            AsyncImage(url: imageURL)
-                                .frame(
-                                    width: maxWidth,
-                                    height: maxHeight
-                                )
-                                .clipShape(
-                                    .rect.size(
-                                        CGSize(
-                                            width: maxWidth,
-                                            height: maxHeight
-                                        )
-                                    )
-                                )
-                                .overlay {
-                                    LinearGradient(
-                                        colors: [
-                                            .clear,
-                                            Color(.systemBackground),
-                                        ],
-                                        startPoint: .top,
-                                        endPoint: .bottom
-                                    )
-                                }
-                                .ignoresSafeArea()
-                        } else {
-                            LinearGradient(
-                                colors: [
-                                    .primary,
-                                    .clear,
-                                ],
-                                startPoint: .top,
-                                endPoint: .bottom
-                            )
+        GeometryReader { geometry in
+            let maxWidth = geometry.size.width
+            let maxHeight = geometry.size.height * 0.4
+            var maxImageWidth = UIScreen.main.bounds.size.width
+            VStack {
+                ZStack(alignment: .bottom) {
+                    if let imageURL = viewModel.getImageURl() {
+                        AsyncImage(url: imageURL)
                             .frame(
-                                width: maxWidth,
+                                width: maxImageWidth,
                                 height: maxHeight
                             )
                             .clipShape(
                                 .rect.size(
                                     CGSize(
-                                        width: maxWidth,
+                                        width: maxImageWidth,
                                         height: maxHeight
                                     )
                                 )
                             )
-                            .ignoresSafeArea()
-                        }
-                        VStack {
-                            Text(viewModel.getTitle())
-                                .font(.title)
-                                .bold()
-                                .fixedSize(
-                                    horizontal: false,
-                                    vertical: true
+                            .overlay {
+                                LinearGradient(
+                                    colors: [
+                                        .clear,
+                                        Color(.systemBackground),
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom
                                 )
-                                .shadow(
-                                    color: Color(.systemBackground),
-                                    radius: 5.0
-                                )
-                                .padding(.bottom, 10.0)
-                            Text(viewModel.getAuthor())
-                                .font(.subheadline)
-                                .foregroundStyle(.gray)
-                            Text(
-                                viewModel.getDatePublished()
-                            )
-                            .font(.caption)
-                            .foregroundStyle(.gray)
-                        }
-                        .padding(.horizontal)
-                    }
-                    .frame(
-                        width: maxWidth,
-                        height: maxHeight
-                    )
-                    if !viewModel.getDescription().isEmpty {
-                        Text(viewModel.getDescription())
-                            .font(.caption)
-                            .padding(.horizontal)
-                            .padding()
-                    }
-                    if !viewModel.getContent().isEmpty {
-                        Text(viewModel.getContent())
-                            .font(.body)
-                            .padding(.horizontal)
-                    }
-                    Spacer()
-                    HStack(alignment: .center) {
-                        if let url = viewModel.getArticleURL() {
-                            Link(destination: url) {
-                                Text("Read full article")
-                                    .font(.title3)
                             }
+                            .ignoresSafeArea()
+                    } else {
+                        LinearGradient(
+                            colors: [
+                                .primary,
+                                .clear,
+                            ],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                        .frame(
+                            width: maxWidth,
+                            height: maxHeight
+                        )
+                        .clipShape(
+                            .rect.size(
+                                CGSize(
+                                    width: maxWidth,
+                                    height: maxHeight
+                                )
+                            )
+                        )
+                        .ignoresSafeArea()
+                    }
+                    VStack {
+                        Text(viewModel.getTitle())
+                            .font(.title)
+                            .bold()
+                            .shadow(
+                                color: Color(.systemBackground),
+                                radius: 5.0
+                            )
+                            .padding()
+                            .frame(width: maxWidth)
+                        Text(viewModel.getAuthor())
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                        Text(
+                            viewModel.getDatePublished()
+                        )
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                    }
+                    .padding(.horizontal)
+                }
+                .frame(
+                    width: maxWidth,
+                    height: maxHeight
+                )
+                if !viewModel.getDescription().isEmpty {
+                    Text(viewModel.getDescription())
+                        .font(.caption)
+                        .padding(.horizontal)
+                        .padding()
+                }
+                if !viewModel.getContent().isEmpty {
+                    Text(viewModel.getContent())
+                        .font(.body)
+                        .padding(.horizontal)
+                }
+                Spacer()
+                HStack(alignment: .center) {
+                    if let url = viewModel.getArticleURL() {
+                        Link(destination: url) {
+                            Text("Read full article")
+                                .font(.title3)
                         }
                     }
-                    .padding()
                 }
+                .padding()
             }
         }
     }
